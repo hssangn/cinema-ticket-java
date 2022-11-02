f4d5039ac6da15b30010ed648d9387e524aa7aa6
hssangn <wrsang@gmail.com>
2022-11-02 00:26:16 +0000

    Update TicketServiceImpl.java
    Remove unused library

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException.*;
 
